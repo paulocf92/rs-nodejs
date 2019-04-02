48a9eaac0d264102f481e1b95dc5be460c4ca4e2
Paulo Filho <pcf.feat@gmail.com>
2019-04-02 13:07:07 -0400

    adding mongoose to handle db operations

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o app
 const app = express();
 
+// Iniciando o DB
+mongoose.connect(
+  "mongodb://192.168.99.100:27017/nodeapi",
+  { useNewUrlParser: true }
+);
+
+// Primeira rota
 app.get('/', (req, res) => {
   res.send('Hello RocketSeat!!');
 });
